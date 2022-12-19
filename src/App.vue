3e4a369083e4c2f118a674221c7473071904c8b0
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2022-12-19 23:25:19 +0500

    GameUpload

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>WebMentor Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Result from './components/Result.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Result
+  },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true;
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        Click Me
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'delay'
+    ],
+    data(){
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted(){
+        // console.log('Component Mounted!')
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer()
+            // console.log(this.delay);
+        }, this.delay);
+    },
+    methods: {
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10);
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            // console.log(this.reactionTime)
+            this.$emit('end', this.reactionTime)
+        }
+    }
+    // updated(){
+    //     console.log("Component updated!")
+    // },
+    // unmounted(){
+    //     console.log("Component Unmounted!");
+    // }
+}
+</script>
+
+<style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        align-items: center;
+        padding: 100px 0px;
+        margin: 40px auto;
+    }
+    button{
+        padding: 10px 30px;
+        background: #333;
+        color: white;
+        font-weight: 600;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+    button[disabled]{
+        opacity: 0.2;
+        cursor: not-allowed;
+    }
+</style>
